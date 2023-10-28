8d516f0f1c5ae0fc1c22aabcb4bb152280e048d1
Alex Kravchenko <alexkravchenko1605@gmail.com>
2023-10-28 14:05:12 +0300

    feat:add main functionality and styles

--- react.components/src/main.tsx
+++ react.components/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '../src/assets/styles/Main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <div className="main_css">
     <App />
-    <App />
-  </React.StrictMode>
+  </div>
 );
