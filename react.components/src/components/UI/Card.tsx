3afb0ca058163894290d07ab500e701dd6c29495
Alex Kravchenko <alexkravchenko1605@gmail.com>
2023-10-31 11:26:32 +0300

    feat: change class  Card component to functional component

--- react.components/src/components/UI/Card.tsx
+++ react.components/src/components/UI/Card.tsx
@@ -1,19 +1,13 @@
-import { Component } from 'react';
 import { Planetdescription } from '../../assets/types';
 
-class Card extends Component<Planetdescription> {
-  constructor(props: Planetdescription) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="card__item">
-        <p>Planet name - {this.props.name}</p>
-        <p>Population - {this.props.population}</p>
-        <p>Climate - {this.props.climate}</p>
-      </div>
-    );
-  }
-}
+const Card = (props: Planetdescription) => {
+  return (
+    <div className="card__item">
+      <p>Planet name - {props.name}</p>
+      <p>Population - {props.population}</p>
+      <p>Climate - {props.climate}</p>
+    </div>
+  );
+};
 
 export default Card;
