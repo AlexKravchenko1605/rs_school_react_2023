94232fb16b06d3cf39a2ef76596f3745ebd85546
Alex Kravchenko <alexkravchenko1605@gmail.com>
2023-10-31 11:28:38 +0300

    feat: change class MyInput component to functional component

--- react.components/src/components/UI/MyInput.tsx
+++ react.components/src/components/UI/MyInput.tsx
@@ -1,23 +1,17 @@
-import React, { Component } from 'react';
 import { Parentsprops } from '../../assets/types';
 
-class MyInput extends Component<Parentsprops> {
-  constructor(props: Parentsprops) {
-    super(props);
-  }
-  render() {
-    return (
-      <form className="text-field__group" onSubmit={this.props.updateData}>
-        <input
-          type="input"
-          placeholder={this.props.placeholder!}
-          className="text-field__input"
-          onChange={this.props.updateName}
-        />
-        <button className="text-field__btn">Find</button>
-      </form>
-    );
-  }
-}
+const MyInput = (props: Parentsprops) => {
+  return (
+    <form className="text-field__group" onSubmit={props.updateData}>
+      <input
+        type="input"
+        placeholder={props.placeholder!}
+        className="text-field__input"
+        onChange={props.updateName}
+      />
+      <button className="text-field__btn">Find</button>
+    </form>
+  );
+};
 
 export default MyInput;
