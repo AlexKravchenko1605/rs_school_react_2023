36970dc08c7da654917efcf62d0058ce7343d2df
Alex Kravchenko <alexkravchenko1605@gmail.com>
2023-11-26 19:45:06 +0300

    refact:"remove all files"

--- react.components/src/store/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import stateReducer from './stateSlice';
-import { planetAPI } from './planetsAPI';
-export const store = configureStore({
-  reducer: { state: stateReducer, [planetAPI.reducerPath]: planetAPI.reducer },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(planetAPI.middleware),
-});
